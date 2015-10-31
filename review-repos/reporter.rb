2ce82cd70764311f7e215abd56821d6b98e419fc
Henri Yandell <hyandell@amazon.com>
2015-10-30 21:38:21 -0700

    API for report plugins

---
+++ review-repos/reporter.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'yaml'
+
+class Reporter
+
+  # exclude and pattern are regexps
+  def filename_check(repo, dir, pattern, name, exclude=nil)
+      sliceIdx=dir.length + 1
+      Dir.glob("#{dir}/**/*").grep(pattern).each do |file|
+        if(exclude and file.match(exclude))
+          next
+        end
+  
+        unless(File.directory?(file))
+          puts "      <warning type='#{name}'>#{file.to_s[sliceIdx..-1]}</warning>"
+        end
+      end
+  end
+  
+  
+  def file_search(repo, dir, pattern, name)
+      warnings=[]
+      Dir.glob("#{dir}/**/*").each do |file|
+        unless(File.directory?(file))
+          if(File.exists?(file))
+            fh=open(file, "r:ASCII-8BIT")
+            num=0
+            fh.each_line do |line|
+              num=num+1
+              if(pattern.match(line))
+                sliceIdx=dir.length + 1
+                escaped=line.chomp.gsub(/&/, "&amp;").gsub(/</, "&lt;").gsub(/>/, "&gt;")
+                puts "      <warning type='#{name}' lineno='#{num}' file='#{file.to_s[sliceIdx..-1]}'>#{escaped}</warning>"
+              end
+            end
+            fh.close
+          end
+        end
+      end
+  end
+
+  # intended to be overridden
+  def report(repo, dir)
+    raise "No report(repo, dir) function defined by report subclass"
+  end
+  
+end
