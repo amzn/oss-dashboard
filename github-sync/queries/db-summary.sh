e2353e7e1e08de1d7db44eff518bfade4f442ce9
Henri Yandell <hyandell@amazon.com>
2015-10-31 02:39:49 -0700

    Have to pass the db in as an argument now as the location is undefined in shell

--- github-sync/queries/db-summary.sh
+++ github-sync/queries/db-summary.sh
@@ -1,6 +1,6 @@
-echo "SELECT COUNT(*) as EventData FROM events;" | sqlite3 --header db/gh-sync.db
-echo "SELECT COUNT(*) as ReleaseData FROM releases;" | sqlite3 --header db/gh-sync.db
-echo "SELECT COUNT(*) as CommitData FROM commits;" | sqlite3 --header db/gh-sync.db
-echo "SELECT COUNT(*) as IssueData FROM issues;" | sqlite3 --header db/gh-sync.db
-echo "SELECT COUNT(*) as PullRequestData FROM pull_requests;" | sqlite3 --header db/gh-sync.db
-echo "SELECT COUNT(*) as PullRequestFileData FROM pull_request_files;" | sqlite3 --header db/gh-sync.db
+echo "SELECT COUNT(*) as EventData FROM events;" | sqlite3 --header $1
+echo "SELECT COUNT(*) as ReleaseData FROM releases;" | sqlite3 --header $1
+echo "SELECT COUNT(*) as CommitData FROM commits;" | sqlite3 --header $1
+echo "SELECT COUNT(*) as IssueData FROM issues;" | sqlite3 --header $1
+echo "SELECT COUNT(*) as PullRequestData FROM pull_requests;" | sqlite3 --header $1
+echo "SELECT COUNT(*) as PullRequestFileData FROM pull_request_files;" | sqlite3 --header $1
