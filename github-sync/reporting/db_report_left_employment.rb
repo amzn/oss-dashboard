a602c3351eb89f56d66e7fceded8814d6a49e480
Henri Yandell <hyandell@amazon.com>
2015-11-25 09:43:35 -0800

    Adding a report to show those who flag (per separate custom script) as having left the company

---
+++ github-sync/reporting/db_report_left_employment.rb
@@ -0,0 +1,42 @@
+# Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'rubygems'
+require 'yaml'
+require_relative 'db_reporter'
+
+class LeftEmploymentDbReporter < DbReporter
+
+  def name()
+    return "Left Employment"
+  end
+
+  def describe()
+    return "This report shows which of the organization members are flagging as having left the company (per a custom task's updating of the users.is_employee column. "
+  end
+
+  def db_columns()
+    return [ ['login', 'member'] ]
+  end
+
+  def db_report(org, sync_db)
+    unknown=sync_db.execute("SELECT DISTINCT(m.login) FROM member m, repository r, team_to_member ttm, team_to_repository ttr WHERE m.login IN (SELECT login FROM users WHERE is_employee=0) AND m.id=ttm.member_id AND ttm.team_id=ttr.team_id AND ttr.repository_id=r.id AND r.org=?", [org])
+    text = ''
+    unknown.each do |row|
+      text << "  <db-reporting type='LeftEmploymentDbReporter'>#{row[0]}</db-reporting>\n"
+    end
+    return text
+  end
+
+end
