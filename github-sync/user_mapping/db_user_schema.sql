24b6f507c8fba1f6335c371c2f3b28c8e97d9684
Henri Yandell <hyandell@amazon.com>
2015-12-14 22:10:23 -0800

    Adding comment on values for is_employee

--- github-sync/user_mapping/db_user_schema.sql
+++ github-sync/user_mapping/db_user_schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS users;
 
+-- is_employee=1 means an employee, is_employee=0 means ex-employee, is_employee=null means unknown
 CREATE TABLE users (
     login VARCHAR,
     email VARCHAR,
