d1d56fece1cde1163aa89c80f065fa46dc5bc69c
Amit Hetawal <amit.hetawal@gmail.com>
2017-01-22 15:20:34 -0800

    Fixing phase : 'github-sync/releases' for sql errors.

--- github-sync/db_releases/sync-releases.rb
+++ github-sync/db_releases/sync-releases.rb
@@ -20,7 +20,7 @@ require 'yaml'
 def db_insert_releases(db, org, repo, releases)
   releases.each do |release|
       db[
-       "DELETE FROM releases WHERE org=? AND repo=? AND id=?", [org, repo, release.id] ]
+       "DELETE FROM releases WHERE org=? AND repo=? AND id=?", org, repo, release.id.to_s].delete
 
       # Sometimes there is no author. Instead, fill in the data with the first file's uploader
       if(release.author)
@@ -40,34 +40,39 @@ def db_insert_releases(db, org, repo, releases)
           org, repo, id, html_url, tarball_url, zipball_url, tag_name, name, body, created_at, published_at, author
         )
         VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? )",
-        [ org, repo, release.id, release.html_url, release.tarball_url, release.zipball_url, release.tag_name, release.name, release.body, release.created_at.to_s, release.published_at.to_s, author]]
+        org, repo, release.id, release.html_url, release.tarball_url, release.zipball_url, release.tag_name,
+        release.name, release.body, release.created_at.to_s, release.published_at.to_s, author].insert
   end
 end
 
 def sync_releases(context, sync_db)
-  
+
   owners = context.dashboard_config['organizations+logins']
   context.feedback.puts " releases"
-  
+
   owners.each do |org|
-    sync_db.execute("BEGIN TRANSACTION");
-    context.feedback.print "  #{org} "
+    begin
+      sync_db.transaction do
+        context.feedback.print "  #{org} "
 
-    if(context.login?(org))
-      repos=context.client.repositories(org)
-    else
-      repos=context.client.organization_repositories(org)
+        if(context.login?(org))
+          repos=context.client.repositories(org)
+        else
+          repos=context.client.organization_repositories(org)
+        end
+
+        # There's no @since here, so it's removing current data and replacing with all release info from GitHub
+        # Could use this for initial load and use the event data stream for updates
+        repos.each do |repo_obj|
+          releases=context.client.releases(repo_obj.full_name)
+          db_insert_releases(sync_db, org, repo_obj.name, releases)   # Replaces existing with these
+          context.feedback.print '.'
+        end
+      end
+    rescue => e
+      puts "Error during processing: #{$!}"
+      puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
     end
-
-    # There's no @since here, so it's removing current data and replacing with all release info from GitHub
-    # Could use this for initial load and use the event data stream for updates
-    repos.each do |repo_obj|
-      releases=context.client.releases(repo_obj.full_name)
-      db_insert_releases(sync_db, org, repo_obj.name, releases)   # Replaces existing with these
-      context.feedback.print '.'
-    end
-    sync_db.execute("END TRANSACTION");
-
     context.feedback.print "\n"
   end
 
