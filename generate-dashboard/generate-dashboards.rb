eb85edac41585f3c63a3a33eb46cc719ed6ffef3
Henri Yandell <hyandell@amazon.com>
2017-09-06 21:47:41 -0700

    Adding src header

--- generate-dashboard/generate-dashboards.rb
+++ generate-dashboard/generate-dashboards.rb
@@ -1,3 +1,16 @@
+# Copyright 2017 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 # TODO: Figure out how to output team+merged data separately
 # Merged is a case of combining the json files neh?
@@ -12,9 +25,6 @@
 #   user-reports less easy as those are more specific to Team/Organizations. 
 #   perhaps simplest to ditch user-reports. Everything becomes about repos, not orgs/teams.
 # This allows for custom dashboard collections.
-# 
-# 
-# 
 # 
 # 
 # New Dashboard Generation
