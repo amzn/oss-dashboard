dbc8d856a4cf4afa4e76c3f99fec3b617c7584f0
Henri Yandell <hyandell@amazon.com>
2017-12-23 17:22:41 -0800

    Make sure the field has data, otherwise it weirds out the xslt

--- db/reporting/db_report_publish.rb
+++ db/reporting/db_report_publish.rb
@@ -31,7 +31,7 @@ class PublishDbReporter < DbReporter
   end
 
   def db_columns()
-    return [ 'Date', ['repository', 'org/repo'], 'Web Traffic First Week', 'Git Clones First Week' ]
+    return [ 'Date', ['Repository', 'org/repo'], 'Web Traffic First Week', 'Git Clones First Week' ]
   end
 
   def db_report(context, repo, sync_db)
@@ -42,20 +42,21 @@ class PublishDbReporter < DbReporter
     end
 
     text = ''
-    text << "  <reporting class='repo-report' repo='#{repo.full_name}' type='PublishDbReporter'>"
-    text << "    <field>#{event[:created_at]}</field>"
-    text << "    <field>#{repo.full_name}</field>"
+    text << "  <reporting class='repo-report' repo='#{repo.full_name}' type='PublishDbReporter'><field>#{event[:created_at]}</field><field>#{repo.full_name}</field>"
 
     ['views', 'clones'].each do |table|
       # To do weekly, need to look at the modulo of the recorded_at: EXTRACT(days FROM (recorded_at - '2017-12-04 23:28:06'))::integer % 4
       views_by_day=sync_db["SELECT uniques FROM traffic_#{table}_total WHERE org=? and repo=? and recorded_at > ? ORDER BY recorded_at LIMIT 7", repo.owner.login, repo.name, event[:created_at] ].all
 
       uniques=views_by_day.map { |row| row[:uniques] }
-
-      text << "<field>#{uniques.join(',')}</field>"
+      if(uniques.empty?)
+        text << "<field>n/a</field>"
+      else
+        text << "<field>#{uniques.join(',')}</field>"
+      end
     end
 
-    text << "  </reporting>\n"
+    text << "</reporting>\n"
 
     return text
   end
